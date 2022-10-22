4aaaa0c29a2e296005c940db390ab1b41ab0cf43
Ivy@RecompiledBirds <fiveggamer@gmail.com>
2022-10-22 15:31:14 -0400

    Initial commit

---
+++ Source/HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using Verse;
+
+namespace RimValiFFARW
+{
+    [StaticConstructorOnStartup]
+    public static class HelloWorld
+    {
+        static HelloWorld()
+        {
+            Log.Message($"<color=orange>[RimValiFFARW]</color> Hello world!");
+        }
+    }
+}
