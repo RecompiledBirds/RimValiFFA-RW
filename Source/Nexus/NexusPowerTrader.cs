8918030ce8488bc7e8ab18b37a1912baa5197a6c
Ivy@RecompiledBirds <fiveggamer@gmail.com>
2023-01-18 17:15:21 -0500

    NexusPowerTrader

---
+++ Source/Nexus/NexusPowerTrader.cs
@@ -0,0 +1,35 @@
+﻿using RimWorld;
+using System.Linq;
+using Verse;
+
+namespace RimValiFFARW.Nexus
+{
+    public class NexusPowerTraderProperties : CompProperties
+    {
+        public NexusPowerTraderProperties()
+        {
+            this.compClass=typeof(NexusPowerTrader);
+        }
+    }
+    public class NexusPowerTrader : CompPowerTrader
+    {
+        public new bool PowerOn
+        {
+            get
+            {
+                if (PowerNet.powerComps.Any(x => x.parent.def == NexusDefOf.RVFFA_AvaliNexus && x.PowerOn))
+                    return base.PowerOn;
+                return false;
+            }
+            set
+            {
+                if (PowerNet.powerComps.Any(x => x.parent.def == NexusDefOf.RVFFA_AvaliNexus && x.PowerOn))
+                {
+                    base.PowerOn = value;
+                }
+                base.PowerOn = false;
+            }
+        }
+        
+    }
+}
