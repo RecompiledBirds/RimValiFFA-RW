9636c4b07928baaf9745d510cb786839df605b87
Danimineiro <daniel_plaster@live.de>
2023-01-22 06:22:21 +0100

    Added WIP PackInspectionTab

---
+++ Source/Packs/PackInspectionWindow.cs
@@ -0,0 +1,28 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace RimValiFFARW.Packs
+{
+    public class PackInspectionWindow : ITab
+    {
+        public readonly Vector2 WinSize = new Vector2(700, 400);
+
+        public override bool IsVisible => base.IsVisible;
+
+        public PackInspectionWindow()
+        {
+            size = WinSize;
+            labelKey = "RVFFA_PackInspectionWindow_LabelKey";
+        }
+
+        protected override void FillTab()
+        {
+            
+        }
+    }
+}
