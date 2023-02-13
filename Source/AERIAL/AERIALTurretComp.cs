7a05f14d53e3ea679d69ae92246ecc3295d4653f
Ivy@RecompiledBirds <52045489+RecompiledBirds@users.noreply.github.com>
2023-02-13 17:24:47 -0500

    AERIAL

---
+++ Source/AERIAL/AERIALTurretComp.cs
@@ -0,0 +1,32 @@
+﻿using RimValiFFARW.AERIAL;
+using System.Collections.Generic;
+using Verse;
+using Verse.AI;
+
+namespace RimValiFFARW
+{
+    public class AERIALTurretComp : ThingComp
+    {
+        public AERIALProps Props => (AERIALProps)props;
+
+        public override IEnumerable<FloatMenuOption> CompFloatMenuOptions(Pawn pawn)
+        {
+            if (!pawn.RaceProps.ToolUser)
+            {
+                yield break;
+            }
+
+            if (!pawn.CanReserveAndReach(parent, PathEndMode.InteractionCell, Danger.Deadly))
+            {
+                yield break;
+            }
+
+            var floatMenuOption = new FloatMenuOption("RefuelThing".Translate(parent.LabelShort, parent), delegate
+            {
+                Job job = JobMaker.MakeJob(AERIAL_DefOfs.RVFFA_RefuelAERIAL, parent);
+                pawn.jobs.TryTakeOrderedJob(job, JobTag.Misc);
+            });
+            yield return floatMenuOption;
+        }
+    }
+}
---
+++ Source/AERIAL/AERIAL_DefOfs.cs
@@ -0,0 +1,16 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RimValiFFARW.AERIAL
+{
+    [DefOf]
+    public static class AERIAL_DefOfs
+    {
+        public static JobDef RVFFA_RefuelAERIAL;
+    }
+}
