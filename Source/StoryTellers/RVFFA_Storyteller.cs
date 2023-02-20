7bd721a378d716665632f0644142d3de63cc891e
Ivy@RecompiledBirds <52045489+RecompiledBirds@users.noreply.github.com>
2023-02-19 20:39:56 -0500

    storyteller comp

---
+++ Source/StoryTellers/RVFFA_Storyteller.cs
@@ -0,0 +1,264 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RimValiFFARW.StoryTellers
+{
+    public class RVFFA_Storyteller : StorytellerCompProperties
+    {
+        public bool dislikesSlaves;
+
+        public bool dislikesNonAvali;
+
+        public int ratioDislikedPawns;
+
+        public SimpleCurve acceptFractionByDaysPassedCurve;
+
+        public SimpleCurve acceptPercentFactorPerProgressScoreCurve;
+
+        public SimpleCurve acceptPercentFactorPerThreatPointsCurve;
+
+        public int maxIncidents = 5;
+
+        public int minIncidents = 1;
+
+        public float minSpacingDays;
+
+        public float offDays;
+        public float onDays;
+
+        public RVFFA_Storyteller()
+        {
+            compClass = typeof(RVFFA_StoryTellerComp);
+        }
+    }
+    public class RVFFA_StoryTellerComp : StorytellerComp
+    {
+        private int index;
+        private int dislikedPawns;
+        private int likedPawns;
+
+        public bool HasDislikedPawns
+        {
+            get
+            {
+                return dislikedPawns > 0;
+            }
+        }
+
+        public int RatioLikedDisliked
+        {
+            get
+            {
+                return likedPawns / dislikedPawns;
+            }
+        }
+
+        public int RatioDislikedLiked
+        {
+            get
+            {
+                return dislikedPawns / likedPawns;
+            }
+        }
+
+        private Random random;
+        public override void Initialize()
+        {
+            index = Find.Storyteller.storytellerComps.IndexOf(this);
+            random = new Random(Find.World.ConstantRandSeed);
+            base.Initialize();
+        }
+
+        private RVFFA_Storyteller Props
+        {
+            get
+            {
+                return props as RVFFA_Storyteller;
+            }
+        }
+
+        public override IEnumerable<FiringIncident> MakeIntervalIncidents(IIncidentTarget target)
+        {
+            Map map = Find.Maps.Find(x => x.Tile == target.Tile);
+            UpdatePawnCount(map);
+
+            float num = 1;
+
+            StorytellerData storytellerData = StorytellerData.GetStorytellerData;
+
+            if(Props.acceptFractionByDaysPassedCurve != null)
+            {
+                num *= Props.acceptFractionByDaysPassedCurve.Evaluate(GenDate.DaysPassedFloat);
+            }
+
+            if (Props.acceptPercentFactorPerThreatPointsCurve != null)
+            {
+                num *= Props.acceptPercentFactorPerThreatPointsCurve.Evaluate(
+                    StorytellerUtility.DefaultThreatPointsNow(target));
+            }
+
+            if (Props.acceptPercentFactorPerProgressScoreCurve != null)
+            {
+                num *= Props.acceptPercentFactorPerProgressScoreCurve.Evaluate(
+                    StorytellerUtility.GetProgressScore(target));
+            }
+
+            int incCount = IncidentCycleUtility.IncidentCountThisInterval(target, index, Props.minDaysPassed, Props.onDays, Props.offDays, Props.minSpacingDays, Props.minIncidents, Props.maxIncidents, num);
+
+            for(int i =0; i < incCount; i++)
+            {
+                switch (storytellerData.StorytellerState)
+                {
+                    case StorytellerState.Angered:
+                        yield return GenAngry(target);
+                        break;
+
+                    case StorytellerState.Neutral:
+                        yield return GenNeutral(target);
+                        break;
+
+                    case StorytellerState.Friendly:
+                        yield return GenFriend(target);
+                        break;
+                }
+            }
+
+            UpdateState(storytellerData);
+        }
+
+        public int GetDislikedPawns(Map map)
+        {
+            IEnumerable<Pawn> pawns = map.mapPawns.FreeColonistsAndPrisonersSpawned;
+
+            int result = 0;
+
+            if (Props.dislikesNonAvali)
+            {
+                int count = pawns.Where(x => x.def != RVFFA_Defs.RVFFA_Avali).Count() ;
+                result += count;
+            }
+            if (Props.dislikesSlaves)
+            {
+                int count = map.mapPawns.PrisonersOfColonyCount;
+                result += count;
+            }
+            return result;
+        }
+        public int GetLikedPawns(Map map)
+        {
+            IEnumerable<Pawn> pawns = map.mapPawns.FreeColonistsAndPrisonersSpawned;
+
+            int result = 0;
+
+            if (Props.dislikesNonAvali)
+            {
+                int count = pawns.Where(x => x.def == RVFFA_Defs.RVFFA_Avali).Count();
+                result += count;
+            }
+            if (Props.dislikesSlaves)
+            {
+                int count = map.mapPawns.FreeColonistsCount;
+                result += count;
+            }
+            return result;
+        }
+        public void UpdatePawnCount(Map map)
+        {
+            dislikedPawns = GetDislikedPawns(map);
+            likedPawns = GetLikedPawns(map);
+        }
+
+        private FiringIncident GenAngry(IIncidentTarget targ)
+        {
+            IncidentParms parms = this.GenerateParms(IncidentCategoryDefOf.ThreatBig, targ);
+            List<IncidentDef> incidents = base.UsableIncidentsInCategory(IncidentCategoryDefOf.ThreatBig,parms).ToList();
+            incidents.AddRange(UsableIncidentsInCategory(IncidentCategoryDefOf.DiseaseHuman, parms));
+            incidents.AddRange(UsableIncidentsInCategory(IncidentCategoryDefOf.DiseaseAnimal, parms));
+            incidents.AddRange(UsableIncidentsInCategory(IncidentCategoryDefOf.DeepDrillInfestation, parms));
+            IncidentDef incident = incidents.RandomElement();
+
+            if (Rand.Chance(0.5f))
+            {
+                parms.points *= parms.points * RatioLikedDisliked;
+            }
+
+            return new FiringIncident(incident, this, parms);
+        }
+
+        private FiringIncident GenNeutral(IIncidentTarget targ)
+        {
+            IncidentParms parms = this.GenerateParms(IncidentCategoryDefOf.ThreatBig, targ);
+            List<IncidentDef> incidents = base.UsableIncidentsInCategory(IncidentCategoryDefOf.ThreatSmall, parms).ToList();
+            incidents.AddRange(UsableIncidentsInCategory(IncidentCategoryDefOf.DiseaseHuman,parms));
+            incidents.AddRange(UsableIncidentsInCategory(IncidentCategoryDefOf.DiseaseAnimal, parms));
+            incidents.AddRange(UsableIncidentsInCategory(IncidentCategoryDefOf.GiveQuest, parms));
+            incidents.AddRange(UsableIncidentsInCategory(IncidentCategoryDefOf.Misc, parms));
+
+            IncidentDef incident = incidents.RandomElement();
+            if (Rand.Chance(0.2f))
+            {
+                parms.points *= parms.points;
+            }
+
+            return new FiringIncident(incident, this, parms);
+        }
+
+        private FiringIncident GenFriend(IIncidentTarget targ)
+        {
+            IncidentParms parms = this.GenerateParms(IncidentCategoryDefOf.ThreatBig, targ);
+            List<IncidentDef> incidents = base.UsableIncidentsInCategory(IncidentCategoryDefOf.ThreatSmall, parms).ToList();
+            incidents.AddRange(UsableIncidentsInCategory(IncidentCategoryDefOf.FactionArrival, parms));
+            incidents.AddRange(UsableIncidentsInCategory(IncidentCategoryDefOf.OrbitalVisitor, parms));
+            incidents.AddRange(UsableIncidentsInCategory(IncidentCategoryDefOf.GiveQuest, parms));
+            incidents.AddRange(UsableIncidentsInCategory(IncidentCategoryDefOf.Misc, parms));
+            incidents.AddRange(UsableIncidentsInCategory(IncidentCategoryDefOf.ShipChunkDrop, parms));
+
+            IncidentDef incident = incidents.RandomElement();
+            if (Rand.Chance(0.05f))
+            {
+                parms.points *= parms.points * RatioDislikedLiked;
+            }
+
+            return new FiringIncident(incident, this, parms);
+        }
+
+        private void UpdateState(StorytellerData data)
+        {
+            if (!data.HasUpdatedInXDays(5))
+                return;
+
+            if(HasDislikedPawns && data.StorytellerState>=0 && Rand.Chance(0.1f))
+            {
+                data.UpdateDays();
+                data.StorytellerState--;
+                return;
+            }
+
+            if (data.StorytellerState == 0 && Rand.Chance(0.2f))
+            {
+                data.StorytellerState += random.Next(-1, RatioDislikedLiked<0.4f? 1 : 0);
+                data.UpdateDays();
+                return;
+            }
+
+            if(data.StorytellerState==StorytellerState.Angered && Rand.Chance(0.2f))
+            {
+                data.StorytellerState += random.Next(0, RatioDislikedLiked<0.6f? 1 : 0);
+                data.UpdateDays();
+                return;
+            }
+
+            if (data.StorytellerState == StorytellerState.Friendly && Rand.Chance(0.5f))
+            {
+                data.StorytellerState -= random.Next(RatioDislikedLiked<0.1f? 0:-1, 1);
+                data.UpdateDays();
+                return;
+            }
+        }
+    }
+}
---
+++ Source/StoryTellers/StorytellerData.cs
@@ -0,0 +1,56 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RimValiFFARW.StoryTellers
+{
+    public class StorytellerData : WorldComponent
+    {
+        public static StorytellerData GetStorytellerData
+        {
+            get
+            {
+                return Find.World.GetComponent<StorytellerData>();
+            }
+        }
+
+        private StorytellerState state;
+        private int dayLastUpdated;
+
+        public bool HasUpdatedInXDays(int x)
+        {
+            return dayLastUpdated >= x+GenDate.DaysPassed;
+        }
+
+        public void UpdateDays()
+        {
+            dayLastUpdated = GenDate.DaysPassed;
+        }
+
+        public StorytellerState StorytellerState
+        {
+            get
+            {
+                return state;
+            }
+            set
+            {
+                state = value;
+            }
+        }
+        public StorytellerData(World world) : base(world)
+        {
+        }
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref state, nameof(state));
+            Scribe_Values.Look(ref dayLastUpdated,nameof(dayLastUpdated));
+        }
+    }
+}
