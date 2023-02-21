5798792987b33d2fab64d5415206162c43731ba9
Ivy@RecompiledBirds <52045489+RecompiledBirds@users.noreply.github.com>
2023-02-20 23:51:49 -0500

    worker for packmates being in the same room

---
+++ Source/Packs/SharedRoomThought.cs
@@ -0,0 +1,24 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RimValiFFARW.Packs
+{
+    public class SharedRoomThought : ThoughtWorker
+    {
+        protected override ThoughtState CurrentStateInternal(Pawn p)
+        {
+            Packmanager.GetLastActivePackmanager.TryGetPackForPawn(p, out Pack pack);
+            if (pack == null)
+                return ThoughtState.Inactive;
+
+            if (pack.Members.All(x => x.GetRoom() == p.GetRoom())) return ThoughtState.ActiveAtStage(1);
+
+            return ThoughtState.ActiveAtStage(0);
+        }
+    }
+}
