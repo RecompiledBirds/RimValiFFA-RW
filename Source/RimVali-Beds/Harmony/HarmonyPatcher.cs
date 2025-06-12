20bc12ab6b0d6b725448212bc918162f965fd0da
Ivy@RecompiledBirds <52045489+RecompiledBirds@users.noreply.github.com>
2025-06-12 07:51:28 -0400

    1.6 upgrade

---
+++ Source/RimVali-Beds/Harmony/HarmonyPatcher.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using RimWorld;
+using RVCRestructured;
+using Verse;
+
+namespace RimValiFFARW
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyPatcher
+    {
+        static HarmonyPatcher()
+        {
+            Harmony harmony = new("RimvaliFFA.Bed.Patches");
+            RVCLog.Log("Running RimValiFFA Bed Patches");
+            harmony.Patch(AccessTools.Method(typeof(BedUtility), nameof(BedUtility.WillingToShareBed)), postfix: new HarmonyMethod(typeof(BedUtilityPatch), nameof(BedUtilityPatch.Postfix)));
+            RVCLog.Log("RVFFA Patches completed.");
+        }
+
+
+
+    }
+
+}
